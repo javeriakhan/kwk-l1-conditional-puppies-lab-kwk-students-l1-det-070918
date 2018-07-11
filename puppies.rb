4aa8b7e5bc1e64c0756bcc062f7a326ba5b05783
javeriakhan <javeriak1017@gmail.com>
2018-07-11 17:31:30 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -1,14 +1,5 @@
+counter = 
 loop do
-  puts "Now watch me whip"
-  puts "Watch me nae nae"
-  puts "Now watch me whip, whip"
-  puts "Now watch me nae nae"
-  puts "Ooooh, watch me, watch me"
-  puts "Ooooh, watch me, watch me"
-  puts "Ooooh, watch me, watch me"
-  break
-end
-
-puts "Do the stanky leg"
-puts "Do the stanky leg"
-exit
+  counter = counter + 1 
+  
+puts "Iteration #"
